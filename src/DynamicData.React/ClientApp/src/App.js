fe914fbb2222a372293a1b6a7dc630403cc91f7a
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-11-13 09:10:39 -0500

    React project - initial commit

---
+++ src/DynamicData.React/ClientApp/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Home } from './components/Home';
+import { Layout } from './components/Layout';
+import './custom.css';
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+      </Layout>
+    );
+  }
+}
